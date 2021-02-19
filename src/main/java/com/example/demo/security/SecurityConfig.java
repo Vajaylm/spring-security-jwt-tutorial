cea849616a912ae2f24b87189fb53657b0538f3d
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-02-19 12:44:10 +0100

    Modify Security configuration

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -15,9 +24,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers("/auth/signin").permitAll()
-                .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN").anyRequest().denyAll();
+                    .authorizeRequests()
+                    .antMatchers("/auth/signin").permitAll()
+                    .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated()
+                    .antMatchers(HttpMethod.GET, "/me").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN")
+                    .anyRequest().denyAll()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
